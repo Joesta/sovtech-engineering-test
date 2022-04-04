67cd735c6ffa820274f546814f80047da3fe8bbb
Johannes Sebolela <Johanness@knowledgeobjects.com>
2022-04-04 23:33:43 +0200

    first commit

---
+++ src/main/java/co/za/sovtech/assessment/softwareengineeringtest/SoftwareEngineeringTestApplication.java
@@ -0,0 +1,13 @@
+package co.za.sovtech.assessment.softwareengineeringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineeringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareEngineeringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/za/sovtech/assessment/softwareengineeringtest/SoftwareEngineeringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.sovtech.assessment.softwareengineeringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareEngineeringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
